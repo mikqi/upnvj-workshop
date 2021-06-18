84ac1423ffcce3d9d3e9af182642dcc9be94668b
Muhammad Rivki <muhammad.rivki@live.com>
2021-06-18 22:38:11 +0700

    feat: add list template innerhtml

--- hackernews-api/main.js
+++ hackernews-api/main.js
@@ -1,6 +1,32 @@
-import './style.css'
+// https://api.hnpwa.com/v0
+// https://api.hnpwa.com/v0/news/1.json
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+const fetchNewsData = async function () {
+  const res = await fetch('https://api.hnpwa.com/v0/news/1.json')
+  return await res.json()
+}
+
+const listTemplate = (news) => `
+  <div class="news-item">
+    <div class="score">${news.points}</div>
+    <div class="title">
+      <a href="${news.url}">${news.title}</a>
+      <span class="host">${news.domain}</span>
+    </div>
+    <div class="meta">
+      <span class="by">
+        by <a href="">${news.user}</a>
+      </span>
+      <span class="time">${news.time_ago}</span>
+      <span class="comments-link">| <a href="">comments: ${news.comments_count}</a></span>
+    </div>
+  </div>
 `
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const newsContainer = document.getElementById('news-container')
+  const newsData = await fetchNewsData()
+  
+  const newsList = newsData.map(listTemplate)
+  newsContainer.innerHTML = newsList.join('')
+})
