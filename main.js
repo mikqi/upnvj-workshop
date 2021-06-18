f6845cdd6cee9b8679d309fb1b259411ed7c21c2
Muhammad Rivki <muhammad.rivki@live.com>
2021-06-18 18:13:51 +0700

    add sample callback

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ const app = document.getElementById('app')
 btnSearch.addEventListener('click', async (ev) => {
   const formUsername = document.getElementById('username')
 
-  const res = await fetch(`https://api.github.com/users/${formUsername.value}`)
-  const data = await res.json()
+  fetch(`https://api.github.com/users/${formUsername.value}`)
+    .then(res => res.json())
+    .then(data => {
+      app.innerText = JSON.stringify(data, null, 2)
+    })
 
-  app.innerText = JSON.stringify(data, null, 2)
+  // const res = await fetch(`https://api.github.com/users/${formUsername.value}`)
+  // const data = await res.json()
+
+  // app.innerText = JSON.stringify(data, null, 2)
 })
